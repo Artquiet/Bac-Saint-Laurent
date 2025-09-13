45b7c1598cefb40678c810e84629d154188f0f3e
Artquiet <99540366+Artquiet@users.noreply.github.com>
2025-09-13 19:48:50 +0200

    exo 8
    Exercice 8 instructions

---
+++ 02-script-client/02-pratique/02-cours-02/exo/3.2.8/js/main.js
@@ -0,0 +1,32 @@
+// 3.1.8  Écrire  un  algorithme  qui  calcule  
+// le  montant  d'une  facture 
+// comportant  une  seule  ligne  de  facture,
+// connaissant  le  prix  unitaire  de 
+// l'article  et  le  nombre  d'articles  achetés.  
+
+// La  TVA  est  uniformément  de 
+// 21 %.Une  remise  sera  calculée  
+// sur  le  montant  HTVA  d'après  la  règle 
+// suivante:
+
+// ●100 articles et plus: 10%
+// ●1000 articles et plus:
+//     ●20% si le montant HTVA est d'au moins 1000€
+//     ●15% sinon
+
+
+// L'algorithme  affichera  le  nombre  d'articles,
+// le  prix  unitaire, le  montant HTVA sans remise,
+// la remise, le montant HTVA avec remise, le taux de 
+// TVA, le montant de la TVA et le montant TVAC.
+// Exemple :
+
+// FACTURE.
+// Nombre d'articles :
+// Prix unitaire :
+// Montant HTVA avant remise :
+// Remise :
+// Montant HTVA avec remise :
+// TAUX TVA (21%) :
+// Montant TVA :
+// Montant TVAC :
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Bac Saint Laurent
+### Bac Saint Laurent
+
 Cursus Dev Web Bac +2
+
+
+## Contact Prof 
+
+christophe.beck@isl.be fait
+martela@isl-edu.be fait 
+alain.corda@isl.be
