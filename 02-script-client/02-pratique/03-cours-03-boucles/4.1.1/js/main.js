c3564b8f324cd51e7215aa8ab381d09817050477
Artquiet <bourse.kev@gmail.com>
2025-09-19 18:03:48 +0200

    javascript cours
    Algo boucle début

---
+++ 02-script-client/02-pratique/03-cours-03-boucles/4.1.1/js/main.js
@@ -0,0 +1,11 @@
+// 1. Écrivez un algorithme affichant 
+// les dix premières valeurs de la table de 
+// multiplication par 2
+
+let number = 0;
+let i = 0;
+
+for (i = 0; i < 10; i++){
+    number = number + 2;
+    console.log(number);
+}
---
+++ 02-script-client/02-pratique/03-cours-03-boucles/4.1.2/js/main.js
@@ -0,0 +1,14 @@
+// 2. Écrivez un algorithme affichant les dix premières valeurs 
+// de la table de multiplication d’un entier 
+// choisi par l'utilisateur (compris entre 2 et 20)
+
+let number = 0;
+let i = 0;
+
+
+do {
+    number = Number(prompt("veuillez entrer une valeure entre 2 et 20 inclus."));
+    i++;
+} while(number <=1 || number >= 21);
+
+console.log(`Voici la valeure que vous avez entrée : ${number}`);
